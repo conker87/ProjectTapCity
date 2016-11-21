d0183c86c0b9bec70ec950ea5b781d23b3889ca9
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2016-11-21 00:58:10 +0000

    Checking in all current stuff. Mostly images actually.

---
+++ Assets/MoveCameraWithMouse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraWithMouse : MonoBehaviour {
+
+	public float mouseSensitivity = .5f;
+	private Vector3 lastPosition;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			
+			lastPosition = Input.mousePosition;
+
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			
+			Vector3 delta = Input.mousePosition - lastPosition;
+			Vector3.Lerp(lastPosition, new Vector3(-delta.x * mouseSensitivity, -delta.y * mouseSensitivity, 0), 1f);
+
+			lastPosition = Input.mousePosition;
+
+		}
+	}
+}
---
+++ Assets/Scripts/SetSortingLayerToY.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SetSortingLayerToY : MonoBehaviour {
+
+	SpriteRenderer sp;
+	public float offset = 0.00f;
+
+	// Use this for initialization
+	void Start () {
+	
+		sp = GetComponent<SpriteRenderer> ();
+
+	}
+
+	void Update() {
+
+		SetSortingLayer ();
+
+	}
+
+
+	void SetSortingLayer() {
+
+		//sp.sortingOrder = -Mathf.RoundToInt(transform.position.y);
+
+		transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.y + offset);
+
+	}
+}
