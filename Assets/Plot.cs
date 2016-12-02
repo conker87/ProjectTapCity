553eb0f471cd9a6013a16e3bd65993601b5b9ad3
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2016-12-02 01:29:49 +0000

    no message

---
+++ Assets/Plot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plot : MonoBehaviour {
+
+	public Vector3 PlotLocation;
+
+	// Use this for initialization
+	void Start () {
+	
+		PlotLocation = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
